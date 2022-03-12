92c02e38a1b0fb4dce981c7f00c7fc6ec015ddad
Erik Roberts <erik@bugg.co>
2022-03-11 19:11:38 -0500

    Remove index.ts from coverage

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,11 @@ import Animation from "./classes/Animation";
 import Curves from "./classes/Curves";
 import LED from "./classes/LED";
 import LEDArray from "./classes/LEDArray";
+
+/*
+
+No logic should go in this file. Only direct imports and exports.
+
+*/
+
 export { Animation, Curves, LED, LEDArray };
